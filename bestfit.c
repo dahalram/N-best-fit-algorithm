db70eec08b8e3f512cf59f1f12c38b3977a9a195
Ram <ramhari.dahal1@gmail.com>
2018-03-29 07:13:25 -0600

    function skeletons

---
+++ bestfit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *memory_size;
+
+void delete_oldest_batch (void) {
+
+}
+
+void add_to_memory(int batch_num, int size) {
+
+}
+
+void generate_processes(int N) {
+
+}
+
+int main (int argc, char *argv[]) {
+
+	int N = atoi(argv[1]);
+	
+	memory_size = (int*) malloc((sizeof(int) * MAX_CAPACITY));
+	return 0;
+}
